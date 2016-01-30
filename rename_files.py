c4476c483b18c9536c9ad4fc2efb8fd81361f1e5
Bijan Fazeli <bijanfazeli@Bijan-Fazelis-MacBook-Pro.local>
2016-01-29 16:55:40 -0800

    Completed step 1 of retrieving file names

--- rename_files.py
+++ rename_files.py
@@ -1,7 +1,11 @@
+#Import modules
+import os
+
 #Create a function called rename_files()
 def rename_files():
     #(1) Get file names from a folder
-
+    file_list = os.listdir(r"/Users/bijanfazeli/Desktop/Coding/fun_with_github/Python/decoding_secret_messages_with_python/prank")
+    print(file_list)
     #(2) For each file, rename the filename
 
 #Call the rename_files() method
