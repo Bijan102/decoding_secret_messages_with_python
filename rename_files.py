d2c168c9934b2cf07cd53fd1afb0ec3f356f980b
Bijan Fazeli <bijanfazeli@Bijan-Fazelis-MacBook-Pro.local>
2016-01-29 16:37:35 -0800

    Skeleton layout of program with sudo code

---
+++ rename_files.py
@@ -0,0 +1,8 @@
+#Create a function called rename_files()
+def rename_files():
+    #(1) Get file names from a folder
+
+    #(2) For each file, rename the filename
+
+#Call the rename_files() method
+rename_files()
